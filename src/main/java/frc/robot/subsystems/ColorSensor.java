26c66aae68490d4e0fd881ce6194a14d4ac4e04a
CookieCoder <cookiecoder15@gmail.com>
2020-01-21 13:29:15 +1100

    Swapped to enum.

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -1,17 +1,27 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
-import edu.wpi.first.wpilibj.AddressableLED; //LED STUFF
-import edu.wpi.first.wpilibj.AddressableLEDBuffer; //LED STUFF
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.interfaces.ColorSensorInterface;
 
 public class ColorSensor extends Subsystem implements ColorSensorInterface {
+
+  private enum Colour {
+    RED,
+    YELLOW,
+    BLUE,
+    GREEN,
+    UNKNOWN
+  }
+
+
   //Values callibrated using vynl sticker for control panel.
   private final Color BlueTarget = ColorMatch.makeColor(0.147, 0.437, 0.416);
   private final Color GreenTarget = ColorMatch.makeColor(0.189, 0.559, 0.250);
@@ -29,7 +39,7 @@ public class ColorSensor extends Subsystem implements ColorSensorInterface {
    * parameter. The device will be automatically initialized with default 
    * parameters.
    */
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorSensorV3 colourSensor = new ColorSensorV3(i2cPort);
 
   /**
    * A Rev Color Match object is used to register and detect known colors. This can 
@@ -38,77 +48,48 @@ public class ColorSensor extends Subsystem implements ColorSensorInterface {
    * This object uses a simple euclidian distance to estimate the closest match
    * with given confidence range.
    */
-  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final ColorMatch colourMatcher = new ColorMatch();
   
   //private final double kFullClockwise = 1;
   //private final double kFullAntiClockwise = -1;
-  private final double colorSpinnerOff = 0;
-  private double colorSpinnerSpeed = -1;
+  private final double colourSpinnerOff = 0;
+  private double colourSpinnerSpeed = -1;
 
-  public String colorStringPrev = "";
-  public String colorString;
-  public String desiredColor = "";
+  private Colour colourPrev = Colour.UNKNOWN;
+  private Colour colour;
+  private Colour desiredColour = Colour.UNKNOWN;
 
-  public AddressableLED m_led;
-  public AddressableLEDBuffer m_ledBuffer;
-  public int m_rainbowFirstPixelHue = 90;
+  private boolean rotational = false;
 
-  public boolean rotational = false;
+  private Colour startColour;
+  private Colour pairColour;
+  private int rotCount;
 
-  public String startColor;
-  public String pairColor;
-  public int rotCount;
-
-  public boolean correctColor = false;
-  public boolean firstLoop = true;
-  public long spinTime;
+  private final PWMVictorSPX spinner = new PWMVictorSPX(1);
 
   @Override
   public void robotInit() {
-    m_colorMatcher.addColorMatch(BlueTarget);
-    m_colorMatcher.addColorMatch(GreenTarget);
-    m_colorMatcher.addColorMatch(RedTarget);
-    m_colorMatcher.addColorMatch(YellowTarget);
-    m_colorMatcher.addColorMatch(WhiteTarget);
-    /*
-    m_colorMatcher.addColorMatch(kYellowBlue);
-    m_colorMatcher.addColorMatch(kBlueGreen);
-    m_colorMatcher.addColorMatch(kGreenRed);
-    m_colorMatcher.addColorMatch(kRedYellow);*/
-    System.out.println("Started");
+    colourMatcher.addColorMatch(BlueTarget);
+    colourMatcher.addColorMatch(GreenTarget);
+    colourMatcher.addColorMatch(RedTarget);
+    colourMatcher.addColorMatch(YellowTarget);
+    colourMatcher.addColorMatch(WhiteTarget);
     // PWM port 9
     // Must be a PWM header, not MXP or DIO
-    m_led = new AddressableLED(9);
-
-    // Reuse buffer
-    // Default to a length of 60, start empty output
-    // Length is expensive to set, so only set it once, then just update data
-    m_ledBuffer = new AddressableLEDBuffer(30);
-    m_led.setLength(m_ledBuffer.getLength());
-
-    // Set the data
-    m_led.setData(m_ledBuffer);
-    m_led.start();
   }
 
   @Override
   public void teleopPeriodic() {
     //Colour order: (Red, Yellow, Blue, Green) x2
     if (!rotational) {
-      String newDesiredColor = getDesiredColor();
-      if (!newDesiredColor.equals(desiredColor)) {
+      Colour newDesiredColor = Colour.getDesiredColor();
+      if (newDesiredColor != desiredColour) {
         System.out.println("Looking for " + newDesiredColor);
-        desiredColor = newDesiredColor;
+        desiredColour = newDesiredColor;
       }
-      spinner.set(getMotorPower(desiredColor, colorString));
+      spinner.set(getMotorPower(desiredColour, colour));
     } else {
         rotate3_5();
-    }
-    if (m_stick.getRawButtonPressed(6)) {
-      addSpeed();
-    }
-    if (m_stick.getRawButtonPressed(5)) {
-      subSpeed();
     }
     if (m_stick.getRawButtonPressed(10)) {
       rotational = !rotational;
@@ -118,94 +99,79 @@ public class ColorSensor extends Subsystem implements ColorSensorInterface {
   }
 
   private void rotate3_5() {
-    desiredColor = "";
+    desiredColour = Colour.UNKNOWN;
     if (rotCount < 0) {
-      startColor = colorString;
-      switch(startColor) {
-        case "Red":
-          pairColor = "Blue";
+      startColour = colour;
+      switch(startColour) {
+        case RED:
+          pairColour = Colour.BLUE;
           break;
-        case "Green":
-          pairColor = "Yellow";
+        case GREEN:
+          pairColour = Colour.YELLOW;
           break;
-        case "Blue":
-          pairColor = "Red";
+        case BLUE:
+          pairColour = Colour.RED;
           break;
-        case "Yellow":
-          pairColor = "Green";
+        case YELLOW:
+          pairColour = Colour.GREEN;
+          break;
+        case UNKNOWN:
+          pairColour = Colour.UNKNOWN;
           break;
       }
-      System.out.println(startColor + ", " + pairColor);
+      System.out.println(startColour + ", " + pairColour);
       rotCount = 0;
     }
    if (rotCount < 14) { // 3.5 rotations == 14 quarter rotations
     if (rotCount > 12) {
-      spinner.set(colorSpinnerSpeed/-2);
+      spinner.set(colourSpinnerSpeed/-2);
     } else {
-      spinner.set(colorSpinnerSpeed*-1);
+      spinner.set(colourSpinnerSpeed*-1);
     }
    } else {
-    spinner.set(colorSpinnerOff);
-    desiredColor = "";
+    spinner.set(colourSpinnerOff);
+    desiredColour = Colour.UNKNOWN;
     rotational = false;
     System.out.println("Rotational: " + rotational);
    }
-   if ((rotCount % 2 == 0 && colorString == pairColor) || (rotCount % 2 != 0 && colorString == startColor)) {
+   if ((rotCount % 2 == 0 && colour == pairColour) || (rotCount % 2 != 0 && colour == startColour)) {
     rotCount += 1;
     System.out.println(rotCount);
    }
   }
 
-  private double getMotorPower(String desired, String current) {
-    if (desired.equals(current) || desired.equals("")) {
-      if (correctColor = false) {
-        if (firstLoop = true) {
-          long spinTime = System.currentTimeMillis();
-          if (spinner.get() > 0) {
-            firstLoop = false;
-            return 0.1;
-          } else {
-            firstLoop = false;
-            return -0.1;
-          }
-        }
-        if (spinTime - System.currentTimeMillis() < 1000) {
-          return spinner.get();
-        } else {
-          correctColor = true;
-          return colorSpinnerOff;
-        }
-      }
-      return colorSpinnerOff;
+  private double getMotorPower(Colour desired, Colour current) {
+    if (desired == current || desired == Colour.UNKNOWN) {
+      return colourSpinnerOff;
     }
-    if (colorString.equals("Red") && desired.equals("Yellow")) {
-      return colorSpinnerSpeed/-2;
+    if (current == Colour.RED && desired == Colour.YELLOW) {
+      return colourSpinnerSpeed/-2;
     }
-    if (colorString.equals("Yellow") && desired.equals("Blue")) {
-      return colorSpinnerSpeed/-2;
+    if (current == Colour.YELLOW && desired == Colour.BLUE) {
+      return colourSpinnerSpeed/-2;
     }
-    if (colorString.equals("Blue") && desired.equals("Green")) {
-      return colorSpinnerSpeed/-2;
+    if (current == Colour.BLUE && desired == Colour.GREEN) {
+      return colourSpinnerSpeed/-2;
     }
-    if (colorString.equals("Green") && desired.equals("Red")) {
-      return colorSpinnerSpeed/-2;
+    if (current == Colour.GREEN && desired == Colour.RED) {
+      return colourSpinnerSpeed/-2;
     }
-    if (colorString.equals("Yellow") && desired.equals("Red")) {
-      return colorSpinnerSpeed/2;
+    if (current == Colour.YELLOW && desired == Colour.RED) {
+      return colourSpinnerSpeed/2;
     }
-    if (colorString.equals("Blue") && desired.equals("Yellow")) {
-      return colorSpinnerSpeed/2;
+    if (current == Colour.BLUE && desired == Colour.YELLOW) {
+      return colourSpinnerSpeed/2;
     }
-    if (colorString.equals("Green") && desired.equals("Blue")) {
-      return colorSpinnerSpeed/2;
+    if (current == Colour.GREEN && desired == Colour.BLUE) {
+      return colourSpinnerSpeed/2;
     }
-    if (colorString.equals("Red") && desired.equals("Green")) {
-      return colorSpinnerSpeed/2;
+    if (current == Colour.RED && desired == Colour.GREEN) {
+      return colourSpinnerSpeed/2;
     }
-    if (colorString.equals("White")) {
+    if (current == Colour.UNKNOWN) {
       return spinner.get();
     }
-    return colorSpinnerSpeed;
+    return colourSpinnerSpeed;
   }
 
   @Override
@@ -220,118 +186,67 @@ public class ColorSensor extends Subsystem implements ColorSensorInterface {
      * an object is the more light from the surroundings will bleed into the 
      * measurements and make it difficult to accurately determine its color.
      */
-    Color detectedColor = m_colorSensor.getColor();
+    Color detectedColor = colourSensor.getColor();
     
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    ColorMatchResult match = colourMatcher.matchClosestColor(detectedColor);
 
     if (match.color == BlueTarget) {
-      colorString = "Blue";
+      colour = Colour.BLUE;
     } else if (match.color == RedTarget) {
-      colorString = "Red";
+      colour = Colour.RED;
     } else if (match.color == GreenTarget) {
-      colorString = "Green";
+      colour = Colour.GREEN;
     } else if (match.color == YellowTarget) {
-      colorString = "Yellow";
-    } else if (match.color == WhiteTarget) {
-      colorString = "White";
-    }/* else if (match.color == kYellowBlue) {
-      colorString = "YellowBlue";
-    } else if (match.color == kBlueGreen) {
-      colorString = "BlueGreen";
-    } else if (match.color == kGreenRed) {
-      colorString = "GreenRed";
-    } else if (match.color == kRedYellow) {
-      colorString = "RedYellow";
-    }*/ else {
-      colorString = "Unknown";
+      colour = Colour.YELLOW;
+    } else {
+      colour = Colour.UNKNOWN;
     }
     
-    colorString = doubleCheck();
+    colour = doubleCheck();
 
-    if (!colorString.equals(colorStringPrev)) {
+    if (colour != colourPrev) {
       //Colour + Confidence
-      System.out.println(colorString + " " + match.confidence);
+      System.out.println(colour + " " + match.confidence);
       //Colour + Time taken to run test.
       //System.out.println(colorString + " " + (System.currentTimeMillis()-starttime));
-      colorStringPrev = colorString;
+      colourPrev = colour;
     }
-    setColor(colorString);
-    //rainbow();
-    // Set the LEDs
-    m_led.setData(m_ledBuffer);
   }
-  private String doubleCheck() {
-    if (!colorString.equals(colorStringPrev)) {
-      if (colorStringPrev.equals("Green") && spinner.get() < 0 && !colorString.equals("Blue")) {
+  private Colour doubleCheck() {
+    if (colour != colourPrev) {
+      if (colourPrev == Colour.GREEN && spinner.get() < 0 && colour != Colour.BLUE) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Green") && spinner.get() > 0 && !colorString.equals("Red")) {
+      if (colourPrev == Colour.GREEN && spinner.get() > 0 && colour != Colour.RED) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Blue") && spinner.get() < 0 && !colorString.equals("Yellow")) {
+      if (colourPrev == Colour.BLUE && spinner.get() < 0 && colour != Colour.YELLOW) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Blue") && spinner.get() > 0 && !colorString.equals("Green")) {
+      if (colourPrev == Colour.BLUE && spinner.get() > 0 && colour != Colour.GREEN) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Yellow") && spinner.get() < 0 && !colorString.equals("Red")) {
+      if (colourPrev == Colour.YELLOW && spinner.get() < 0 && colour != Colour.RED) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Yellow") && spinner.get() > 0 && !colorString.equals("Blue")) {
+      if (colourPrev == Colour.YELLOW && spinner.get() > 0 && colour != Colour.BLUE) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Red") && spinner.get() < 0 && !colorString.equals("Green")) {
+      if (colourPrev == Colour.RED && spinner.get() < 0 && colour != Colour.GREEN) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
-      if (colorStringPrev.equals("Red") && spinner.get() > 0 && !colorString.equals("Yellow")) {
+      if (colourPrev == Colour.RED && spinner.get() > 0 && colour != Colour.YELLOW) {
         System.out.println("Middle");
-        return colorStringPrev;
+        return colourPrev;
       }
     }
-      return colorString;
-  }
-  
-  private void rainbow() {
-    // For every pixel
-    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      // Calculate the hue - hue is easier for rainbows because the color
-      // shape is a circle so only one value needs to precess
-      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
-      // Set the value
-      m_ledBuffer.setHSV(i, hue, 255, 64);
-    }
-    // Increase by to make the rainbow "move"
-    m_rainbowFirstPixelHue += 3;
-    // Check bounds
-    m_rainbowFirstPixelHue %= 180;
-  }
-  private void setColor(String colour) {
-    // For every pixel
-    if (colour.equals("Red")) {
-      for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-        m_ledBuffer.setRGB(i, 128, 0, 0);
-      }
-    } else if (colour.equals("Green")) {
-      for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-        m_ledBuffer.setRGB(i, 0, 128, 0);
-      }
-    } else if (colour.equals("Blue")) {
-      for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-        m_ledBuffer.setRGB(i, 0, 0, 128);
-      }
-    } else if (colour.equals("Yellow")) {
-      for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-        m_ledBuffer.setRGB(i, 128, 128, 0);
-      }
-    } else {
-      rainbow();
-    }
+      return colour;
   }
 }
