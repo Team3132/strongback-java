c10b45c857066cecf13c70780db906ed33f5f23b
WainTrain <jkwainwright13@gmail.com>
2020-02-21 20:53:38 +1100

    Created initial files for RestartableLog

---
+++ src/main/java/frc/robot/lib/RestartableLog.java
@@ -0,0 +1,13 @@
+package frc.robot.lib;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RestartableLog {
+
+
+    
+}
