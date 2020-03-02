a232d78d2877409acdee0a86afc1b011a2d84dbf
mrwaldron <mark.r.waldron@gmail.com>
2020-03-02 20:43:27 +1100

    Example OI based toggle mode.
    Complicated way of implementing toggles in OI that handles running different
    sequences based on the toggle state.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -97,7 +97,11 @@ public class OI implements OIInterface {
 		onUntriggered(rightStick.getButton(5), Sequences.applyClimberBrake());		
 
 		// Toggle drive / climb mode
-		onTriggered(rightStick.getButton(6), Sequences.toggleDriveClimbModes());
+		// Silly example, don't take it seriously.
+		onToggle(rightStick.getButton(6), "drive/climb", Sequences.enableClimbMode(), Sequences.enableDriveMode())
+		  .onTriggered(rightStick.getButton(5), Sequences.releaseClimberBrake(), Sequences.startSlowDriveForward())
+		  .onUntriggered(rightStick.getButton(5), Sequences.releaseClimberBrake(), Sequences.driveReallyFast());
+   
 
 		// Vision lineup
 		onTriggered(rightStick.getButton(2), Sequences.visionAim());
@@ -234,19 +238,126 @@ public class OI implements OIInterface {
 	private void onUntriggered(Switch swtch, Supplier<Sequence> func) {
 		reactor.onUntriggered(swtch, () -> exec.doSequence(func.get()));
 	}
-	
+
 	@SuppressWarnings("unused")
 	private void whileTriggered(Switch swtch, Runnable func) {
 		reactor.whileTriggered(swtch, func);
-  }
-	
+	}
+
 	@SuppressWarnings("unused")
 	private void whileTriggered(Switch swtch, Sequence seq) {
 		reactor.whileTriggered(swtch, () -> exec.doSequence(seq));
-  }
-	
-  @SuppressWarnings("unused")
-  private void whileTriggered(Switch swtch, Supplier<Sequence> func) {
-	  reactor.whileTriggered(swtch, () -> exec.doSequence(func.get()));
-  }
+	}
+
+	@SuppressWarnings("unused")
+	private void whileTriggered(Switch swtch, Supplier<Sequence> func) {
+		reactor.whileTriggered(swtch, () -> exec.doSequence(func.get()));
+	}
+
+	/**
+	 * Create a toggle switch based on a single button.
+	 * Supports having other buttons change their behaviour based on the state of the toggle.
+	 * 
+	 * <pre>
+     * {@code
+	 * onToggle(rightStick.getButton(6), "drive/climb", Sequences.enableClimbMode(), Sequences.enableDriveMode())
+	 *   .onTriggered(rightStick.getButton(5), Sequences.releaseClimberBrake(), Sequences.startSlowDriveForward())
+	 *   .onUntriggered(rightStick.getButton(5), Sequences.releaseClimberBrake(), Sequences.driveFast());
+     * }
+     * </pre>
+	 * 
+	 * @param swtch condition used to toggle the state. Normally a button press.
+	 * @param name used for logging when the toggle changes.
+	 * @param onSeq sequence to run when the toggle is triggered on.
+	 * @param offSeq sequence to run when the toggle is triggered off.
+	 * @return the ToggleSwitch for further chaining of more buttons based on the toggle state.
+	 */
+	private ToggleSwitch onToggle(Switch swtch, String name, Sequence onSeq, Sequence offSeq) {
+		return new ToggleSwitch(swtch, name, onSeq, offSeq);
+	}
+
+	/**
+	 * Maintains the state of a toggle switch based on a single button.
+	 * Supports having other buttons change their behaviour based on the state of the toggle.
+	 */
+	@SuppressWarnings("unused")
+	private class ToggleSwitch {
+		private boolean toggled = false;
+
+		/**
+		 * Creates a ToggleSwitch to track the state and run sequences on state change.
+		 * 
+		 * @param swtch  condition used to toggle the state. Normally a button press.
+		 * @param name   used for logging when the toggle changes.
+		 * @param onSeq  sequence to run when the toggle is triggered on.
+		 * @param offSeq sequence to run when the toggle is triggered off.
+		 * @return the ToggleSwitch for further chaining of more buttons based on the toggle state.
+		 */
+		public ToggleSwitch(Switch swtch, String name, Sequence onSeq, Sequence offSeq) {
+			Strongback.switchReactor().onTriggered(swtch, () -> {
+				if (!toggled) {
+					log.sub("Toggling on " + name);
+					exec.doSequence(onSeq);
+				} else {
+					log.sub("Toggling off " + name);
+					exec.doSequence(offSeq);
+				}
+				toggled = toggled ^ true;
+			});
+		}
+
+		/**
+		 * Run different sequences depending on toggle state on button press.
+		 * @param swtch condition to trigger a sequence to run. Normally a button press.
+		 * @param ifOnSeq sequence to run if the toggle is on.
+		 * @param ifOffSeq sequence to run if the toggle is off.
+		 * @return the ToggleSwitch for further chaining of more button based on the toggle state.
+		 */
+		public ToggleSwitch onTriggered(Switch swtch, Sequence ifOnSeq, Sequence ifOffSeq) {
+			Strongback.switchReactor().onTriggered(swtch, () -> {
+				if (toggled) {
+					exec.doSequence(ifOnSeq);
+				} else {
+					exec.doSequence(ifOffSeq);
+				}
+			});
+			return this;
+		}
+
+		/**
+		 * Run different sequences depending on toggle state on button release.
+		 * @param swtch condition to trigger a sequence to run. Normally a button release.
+		 * @param ifOnSeq sequence to run if the toggle is on.
+		 * @param ifOffSeq sequence to run if the toggle is off.
+		 * @return the ToggleSwitch for further chaining of more button based on the toggle state.
+		 */
+		public ToggleSwitch onUntriggered(Switch swtch, Sequence ifOnSeq, Sequence ifOffSeq) {
+			Strongback.switchReactor().onUntriggered(swtch, () -> {
+				if (toggled) {
+					exec.doSequence(ifOnSeq);
+				} else {
+					exec.doSequence(ifOffSeq);
+				}
+			});
+			return this;
+		}
+
+		/**
+		 * Run different sequences depending on toggle state while a button is pressed.
+		 * @param swtch condition to trigger a sequence to run. Normally while a button is pressed.
+		 * @param ifOnSeq sequence to run if the toggle is on.
+		 * @param ifOffSeq sequence to run if the toggle is off.
+		 * @return the ToggleSwitch for further chaining of more button based on the toggle state.
+		 */
+		public ToggleSwitch whileTriggered(Switch swtch, Sequence ifOnSeq, Sequence ifOffSeq) {
+			Strongback.switchReactor().whileTriggered(swtch, () -> {
+				if (toggled) {
+					exec.doSequence(ifOnSeq);
+				} else {
+					exec.doSequence(ifOffSeq);
+				}
+			});
+			return this;
+		}
+	}
 }
