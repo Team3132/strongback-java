5c96c24a519c4480bcadb4c32342170c37c8a045
CookieCoder <cookiecoder15@gmail.com>
2020-03-27 19:12:58 +1100

    Changed from onMode to onToggle

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -145,7 +145,7 @@ public class OI implements OIInterface {
 		onUntriggered(stick.getAxis(GamepadButtonsX.RIGHT_TRIGGER_AXIS, GamepadButtonsX.TRIGGER_THRESHOLD), Sequences.stopShooting());
 
 		// Buddy climb
-		onMode(stick.getButton(GamepadButtonsX.B_BUTTON), stick.getButton(GamepadButtonsX.X_BUTTON), "deploybuddyclimb/stowbuddyclimb", Sequences.deployBuddyClimb(), Sequences.stowBuddyClimb());
+		onToggle(stick.getButton(GamepadButtonsX.B_BUTTON), "deploybuddyclimb/stowbuddyclimb", Sequences.deployBuddyClimb(), Sequences.stowBuddyClimb());
 
 		// Stow the intake
 		onTriggered(stick.getButton(GamepadButtonsX.BACK_BUTTON), Sequences.raiseIntake());
