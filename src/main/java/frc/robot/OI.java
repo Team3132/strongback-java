7a21ecf8880c9c4a7c0e8d4bf2cd438eac9ee9d2
CookieCoder <cookiecoder15@gmail.com>
2020-03-27 19:06:09 +1100

    Removed random new line

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -145,7 +145,6 @@ public class OI implements OIInterface {
 		onUntriggered(stick.getAxis(GamepadButtonsX.RIGHT_TRIGGER_AXIS, GamepadButtonsX.TRIGGER_THRESHOLD), Sequences.stopShooting());
 
 		// Buddy climb
-		
 		onMode(stick.getButton(GamepadButtonsX.B_BUTTON), stick.getButton(GamepadButtonsX.X_BUTTON), "deploybuddyclimb/stowbuddyclimb", Sequences.deployBuddyClimb(), Sequences.stowBuddyClimb());
 
 		// Stow the intake
